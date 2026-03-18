9313f6e9d7822c4c5bbbd2379fffe2062bc0f2d6
Luis Moßburger <luis.mossburger@spark-up.de>
2026-03-18 20:19:37 +0100

    Fix

--- catalog/next.config.js
+++ catalog/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: 'cover.ekz.de' },
+      { hostname: 'covers.openlibrary.org' },
+    ],
+  },
+}
 
 module.exports = nextConfig
